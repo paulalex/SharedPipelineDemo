a73f1587a25b45e6d13629e9b601084ee2b1bae2
Paul Ockleford <paul.ockleford@nhs.net>
2020-03-26 09:57:47 +0000

    More changes

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -10,12 +10,6 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
         stages {
-            stage('checkout git') {
-                steps {
-                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
-                }
-            }
-
             stage('build') {
                 steps {
                     sh 'echo building...'
